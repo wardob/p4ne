cccf887b5cc93512e6ddc6e46344389490c5a783
wardob <wardob@gmail.com>
2017-04-13 17:53:28 +0300

    §

---
+++ PycharmProjects/p4ne/Lab1.2/data_analysis.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot
+from openpyxl import load_workbook
+
+def getvalue(x):
+    return x.value
+
+wb = load_workbook('/tmp/data_analysis_lab.xlsx')
+sheet = wb['Data']
+years = list(map(getvalue, sheet['A'][1:]))
+relations = list(map(getvalue, sheet['B'][1:]))
+activity = list(map(getvalue, sheet['C'][1:]))
+
+pyplot.plot(years, relations, label='Отношения')
+pyplot.plot(years, activity, label='Активность Солнца')
+pyplot.xlabel('Годы')
+pyplot.ylabel('Отношения/Активность Солнца')
+pyplot.legend(loc='upper left')
+pyplot.show()
