f8c6b4801debcd19944e281fe663d3a17f4d8f1a
wardob <wardob@gmail.com>
2017-04-14 16:16:02 +0300

    §

---
+++ PycharmProjects/p4ne/Sem1.3/strings_and_loops.py
@@ -0,0 +1,20 @@
+i = 1
+
+def classify(i):
+    if not (i % 2):
+        return ">"
+    elif not (i % 3):
+        return "^"
+    elif not (i % 5):
+        return "&"
+    else:
+        return "?"
+
+while i <= 20:
+    j = 1
+    while j <= 20:
+        print("%s%d" % (classify(i*j), i*j), end=" ")
+        j += 1
+    print("")
+    i += 1
+
